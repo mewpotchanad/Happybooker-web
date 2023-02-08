2d6216d43b9456cb75537c05e15c18b4393d8019
mewpotchanad <natthapotchanad@gmail.com>
2023-02-08 16:19:16 +0700

    login form

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import AuthContextProvider from './contexts/AuthContext'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
