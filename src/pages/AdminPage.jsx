9df7500e266e6189bef3c57c72df70a6abe0b65c
mewpotchanad <natthapotchanad@gmail.com>
2023-03-10 15:23:06 +0700

    create page css

---
+++ src/pages/AdminPage.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+
+export default function AdminPage() {
+  return (
+    <>
+      <div className="bg-white w-full p-4">
+        <div className="font-bold text-xl">Admin page</div>
+      </div>
+      <div className="m-auto w-[70%] mt-10">
+        <div className="border-2 border-white p-10 rounded-md flex justify-center">
+          <div className="flex flex-col gap-4">
+            <div className="pb-4 font-bold text-xl">รายละเอียดหนังสือ</div>
+            <div className="flex gap-2">
+              <div className="w-20">ชื่อหนังสือ</div>
+              <div>
+                <input className="rounded-md w-[800px]" />
+              </div>
+            </div>
+            <div className="flex gap-2">
+              <div className="w-20">หน้าปก</div>
+              <div>
+                <input className="rounded-md w-[800px]" />
+              </div>
+              <button className="bg-[#FEC601] text-white rounded-md px-2">อัพโหลด</button>
+            </div>
+            <div className="flex gap-2">
+              <div className="w-20">นักเขียน</div>
+              <div>
+                <input className="rounded-md w-[800px]" />
+              </div>
+            </div>
+            <div className="flex gap-2">
+              <div className="w-20">สำนักพิมพ์</div>
+              <div>
+                <input className="rounded-md w-[800px]" />
+              </div>
+            </div>
+            <div className="flex gap-2">
+              <div className="w-20">หมวดหมู่</div>
+              <div>
+                <input className="rounded-md w-[800px]" />
+              </div>
+            </div>
+            <div className="flex gap-2">
+              <div className="w-20">เรื่องย่อ</div>
+              <div>
+                <textarea className="rounded-md w-[800px]" />
+              </div>
+            </div>
+            <div className="flex justify-center gap-3">
+              <button className="bg-[#FEC601] text-white rounded-md px-4">ยกเลิก</button>
+              <button className="bg-[#FEC601] text-white rounded-md px-4">บันทึก</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,108 +1,116 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import LoginPage from '../pages/LoginPage';
-import HomePage from '../pages/HomePage';
-import ShelfPage from '../pages/ShelfPage';
-import EbookPage from '../pages/EbookPage';
-import ProfilePage from '../pages/ProfilePage';
-import AccountPage from '../pages/AccountPage';
-import CategoryPage from '../pages/CategoryPage';
-import RegisterPage from '../pages/RegisterPage';
-import NovelPage from '../pages/NovelPage';
-import CartoonPage from '../pages/CartoonPage';
-import SelfhelpPage from '../pages/SelfhelpPage';
-import Navbar from '../layouts/Navbar';
-import RedirectiIfAuthenticate from '../features/auth/RedirectiIfAuthenticate';
-import ProtectedRoute from '../features/auth/ProtectedRoute';
-import EditProfilePage from '../pages/EditProfilePage'
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LoginPage from "../pages/LoginPage";
+import HomePage from "../pages/HomePage";
+import ShelfPage from "../pages/ShelfPage";
+import EbookPage from "../pages/EbookPage";
+import ProfilePage from "../pages/ProfilePage";
+import AccountPage from "../pages/AccountPage";
+import CategoryPage from "../pages/CategoryPage";
+import RegisterPage from "../pages/RegisterPage";
+import NovelPage from "../pages/NovelPage";
+import CartoonPage from "../pages/CartoonPage";
+import SelfhelpPage from "../pages/SelfhelpPage";
+import Navbar from "../layouts/Navbar";
+import RedirectiIfAuthenticate from "../features/auth/RedirectiIfAuthenticate";
+import ProtectedRoute from "../features/auth/ProtectedRoute";
+import EditProfilePage from "../pages/EditProfilePage";
+import AdminPage from "../pages/AdminPage";
 
 const router = createBrowserRouter([
-    {
-        path: '/login',
-        element: (
-            <RedirectiIfAuthenticate>
-                <LoginPage />
-            </RedirectiIfAuthenticate>
-        )
-    }, {
-        path: '/register',
-        element: (
-            <RedirectiIfAuthenticate>
-                <RegisterPage />
-            </RedirectiIfAuthenticate>
-        )
-    }, {
-        path: '/profile',
-        element: (
-            <ProtectedRoute>
-                <ProfilePage />
-            </ProtectedRoute>
-        )
-    }, {
-        path: '/account',
-        element: (
-            <ProtectedRoute>
-                <AccountPage />
-            </ProtectedRoute>
-        )
-    }, {
-        path: '/category',
-        element: (
-            <ProtectedRoute>
-                <CategoryPage />
-            </ProtectedRoute>
-        )
-    }, {
-        path: '/shelf',
-        element: (
-            <ProtectedRoute>
-                <ShelfPage />
-            </ProtectedRoute>
-        )
-    }, {
-        path: '/ebook/:ebookId',
-        element: (
-            <ProtectedRoute>
-                <EbookPage />
-            </ProtectedRoute>
-        )
-    }, {
-        path: '/editprofile',
-        element: (
-            <ProtectedRoute>
-                <EditProfilePage />
-            </ProtectedRoute>
-        )
-    },
+  {
+    path: "/login",
+    element: (
+      <RedirectiIfAuthenticate>
+        <LoginPage />
+      </RedirectiIfAuthenticate>
+    )
+  },
+  {
+    path: "/register",
+    element: (
+      <RedirectiIfAuthenticate>
+        <RegisterPage />
+      </RedirectiIfAuthenticate>
+    )
+  },
+  {
+    path: "/profile",
+    element: (
+      <ProtectedRoute>
+        <ProfilePage />
+      </ProtectedRoute>
+    )
+  },
+  {
+    path: "/account",
+    element: (
+      <ProtectedRoute>
+        <AccountPage />
+      </ProtectedRoute>
+    )
+  },
+  {
+    path: "/category",
+    element: (
+      <ProtectedRoute>
+        <CategoryPage />
+      </ProtectedRoute>
+    )
+  },
+  {
+    path: "/shelf",
+    element: (
+      <ProtectedRoute>
+        <ShelfPage />
+      </ProtectedRoute>
+    )
+  },
+  {
+    path: "/ebook/:ebookId",
+    element: (
+      <ProtectedRoute>
+        <EbookPage />
+      </ProtectedRoute>
+    )
+  },
+  {
+    path: "/editprofile",
+    element: (
+      <ProtectedRoute>
+        <EditProfilePage />
+      </ProtectedRoute>
+    )
+  },
 
-
-    {
-        path: '/',
-        element: (
-            <ProtectedRoute>
-                <Navbar />
-            </ProtectedRoute>
-        ),
-        children: [
-            {
-                path: '/',
-                element: <HomePage />
-            }, {
-                path: '/novel',
-                element: <NovelPage />
-            }, {
-                path: '/cartoon',
-                element: <CartoonPage />
-            }, {
-                path: '/selfhelp',
-                element: <SelfhelpPage />
-            }
-        ]
-    }
-
-
-])
+  {
+    path: "/",
+    element: (
+      <ProtectedRoute>
+        <Navbar />
+      </ProtectedRoute>
+    ),
+    children: [
+      {
+        path: "/",
+        element: <HomePage />
+      },
+      {
+        path: "/novel",
+        element: <NovelPage />
+      },
+      {
+        path: "/cartoon",
+        element: <CartoonPage />
+      },
+      {
+        path: "/selfhelp",
+        element: <SelfhelpPage />
+      }
+    ]
+  },
+  { path: "/admin", element: <AdminPage /> }
+]);
 
 export default function Router() {
-    return <RouterProvider router={router} />
+  return <RouterProvider router={router} />;
 }
